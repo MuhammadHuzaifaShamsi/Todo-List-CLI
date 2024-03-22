bd6d25b80c991b9ff1342e8bfb6fb87e13907843
MuhammadHuzaifaZafar <huzaifashamsi819@gmail.com>
2024-03-22 22:38:27 +0500

    updated files

--- app.js
+++ app.js
@@ -10,15 +10,10 @@ while (true) {
             message: "Enter your task: "
         },
         {
-            name: "deadline",
-            type: "input",
-            message: "Set a deadline for this task (DD-MM-YYYY): "
-        },
-        {
             name: "priority",
             type: "list",
             choices: ["Important and Urgent", "Important", "Urgent"],
-            message: "Categorize it based on its urgency!"
+            message: "Categorize it based on its urgency:"
         },
         {
             name: "addAnother",
@@ -31,8 +26,8 @@ while (true) {
         break;
     }
 }
-console.log(chalk.magentaBright("Categorized tasks:"));
+console.log(chalk.magentaBright("Your Todo List:"));
 for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
-    console.log(`${chalk.blueBright(i + 1)}. Task: ${chalk.green(task.task)}, Deadline: ${chalk.green(task.deadline)}, Priority: ${chalk.green(task.priority)}.`);
+    console.log(`${chalk.blueBright(i + 1)}. Task: ${chalk.green(task.task)}, Priority: ${chalk.green(task.priority)}.`);
 }
--- app.ts
+++ app.ts
@@ -5,7 +5,6 @@ import chalk from "chalk";
 
 interface task {
     task: string;
-    deadline: string;
     priority: string;
 }
 
@@ -19,15 +18,10 @@ while (true) {
             message: "Enter your task: "
         },
         {
-            name: "deadline",
-            type: "input",
-            message: "Set a deadline for this task (DD-MM-YYYY): "
-        },
-        {
             name: "priority",
             type: "list",
             choices: ["Important and Urgent", "Important", "Urgent"],
-            message: "Categorize it based on its urgency!"
+            message: "Categorize it based on its urgency:"
         },
         {
             name: "addAnother",
@@ -44,11 +38,11 @@ while (true) {
 }
 
 
-console.log(chalk.magentaBright("Categorized tasks:"));
+console.log(chalk.magentaBright("Your Todo List:"));
 
 for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
     console.log(
-        `${chalk.blueBright(i + 1)}. Task: ${chalk.green(task.task)}, Deadline: ${chalk.green(task.deadline)}, Priority: ${chalk.green(task.priority)}.`
+        `${chalk.blueBright(i + 1)}. Task: ${chalk.green(task.task)}, Priority: ${chalk.green(task.priority)}.`
     );   
 }
